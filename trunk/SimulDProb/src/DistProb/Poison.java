4be06428b5ebfce7d6a7629e7b952aa65b5ed959
stevevv06 <stevevv06@63594d4b-9826-2389-b0fe-ec1b1543a081>
2010-04-17 16:46:18 +0000

    sdnsdfc

--- trunk/SimulDProb/src/DistProb/Poison.java
+++ trunk/SimulDProb/src/DistProb/Poison.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package DistProb;
 
 /**
@@ -11,15 +10,45 @@ package DistProb;
  */
 public class Poison {
 
-    public static int Poison(double media){
-     int x=0;
-     double k = Math.exp(media*-1);
-     double prod = Math.random();
-     while(prod>=k){
-         x++;
-         prod = Math.random();
-     }
-     return x;
+    double media;
+
+    public Poison(double media) {
+        this.media = media;
     }
-    
+
+    public int Poison() {
+        return Poison(this.media);
+    }
+
+    public int Poison(double media) {
+        int x = 0;
+        double k = Math.exp(media * -1);
+        double prod = Math.random();
+        while (prod >= k) {
+            x++;
+            prod = Math.random();
+        }
+        return x;
+    }
+
+    public int[] getObservaciones(int observaciones) {
+        return getObservaciones(observaciones, this.media);
+    }
+
+    public int[] getObservaciones(int observaciones, double media) {
+        int[] ret = new int[observaciones];
+        for (int i = 0; i < ret.length; i++) {
+            ret[i] = Poison(media);
+        }
+        System.out.println("");
+        return ret;
+    }
+
+    public double getMedia() {
+        return this.media;
+    }
+
+    public double getVarianza() {
+        return this.media;
+    }
 }
